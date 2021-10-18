dff8050f39df9c4909d9133c088421946eba1350
aleucilima <aleuci.lima@gmail.com>
2021-10-18 19:38:38 -0300

    Update in the HomeScreen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-
-import { Container, Title } from './styles'
+import { 
+    Container,
+    Header,
+    UserContainer,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from './styles'
 
 export function Dashboard() {
-    return (
-        <Container>
-            <Title>Dashboard</Title>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Photo source={{ uri: 'http://github.com/aleucilima.png'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>João</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserContainer>
+      </Header>
+    </Container>
+  )
 }
 
