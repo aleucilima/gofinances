374d541f0e927db13d856d642c86469ea3043903
aleucilima <aleuci.lima@gmail.com>
2021-12-02 19:41:34 -0300

    Update in the cards

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -30,15 +30,15 @@ export function HighlightCard({
   lastTransaction 
 } : Props) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>{title}</Title>
+        <Title type={type}>{title}</Title>
         <Icon name={icon[type]} type={type} />
       </Header>
 
       <Footer>
-        <Amount>{amount}</Amount>
-        <LastTransaction>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>
           {lastTransaction}
         </LastTransaction>
       </Footer>
