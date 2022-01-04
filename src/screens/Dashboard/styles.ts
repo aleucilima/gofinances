f109a8ac7d1dd4d118b87e6820a0d5e78bb2bdbc
aleucilima <aleuci.lima@gmail.com>
2022-01-03 21:22:31 -0300

    Update no projeto.

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -95,6 +95,8 @@ export const Title = styled.Text`
 export const TransactionsList = styled(
   FlatList as new () => FlatList<DataListProps>
   ).attrs({
-  showsVerticalScrollIndicator: false,
-  contentContainerStyle: { paddingBottom: getBottomSpace() }
-  })``;
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: { paddingBottom: getBottomSpace() }
+  })`
+  
+`;
