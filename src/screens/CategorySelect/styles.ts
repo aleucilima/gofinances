2cab7a5ea6b9ad2fd55c5f230d119fefe9a1976e
aleucilima <aleuci.lima@gmail.com>
2022-01-13 21:14:09 -0300

    Update no projeto

---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: ${RFValue(19)}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Category = styled.View``;
+export const Icon = styled(Feather)``;
+export const Name = styled.Text``;
--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -39,5 +39,5 @@ export const TransactionsTypes = styled.View`
   flex-direction: row;
   justify-content: space-between;
 
-  margin-top: 8px;
+  margin: 8px 0 16px;
 `;
