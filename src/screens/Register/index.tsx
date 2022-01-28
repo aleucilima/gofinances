4ea1ac88e38c5e1d14c435e110bc16d2989d41a5
aleucilima <aleuci.lima@gmail.com>
2022-01-28 08:13:14 -0300

    Update no projeto, utilizando modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -68,7 +68,7 @@ export function Register() {
           </TransactionsTypes>
 
           <CategorySelectButton
-            title='Categoria'
+            title={category.name}
             onPress={handleOpenSelectCategoryModal}
           />
         </Fields>
@@ -77,7 +77,10 @@ export function Register() {
         />
       </Form>
 
-      <Modal visible={categoryModalOpen}>
+      <Modal 
+        visible={categoryModalOpen}
+        statusBarTranslucent
+      >
         <CategorySelect 
           category={category}
           setCategory={setCategory}
