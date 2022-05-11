d19f88d338b69f19d3c5c860594d5b5cb9b0775e
aleucilima <aleuci.lima@gmail.com>
2022-05-11 20:23:10 -0300

    Update SignIn screen and creation SVG types.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import AppLoading from 'expo-app-loading'
-import { ThemeProvider } from 'styled-components'
-import 'intl'
-import 'intl/locale-data/jsonp/pt-BR'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
-} from '@expo-google-fonts/poppins'
+} from '@expo-google-fonts/poppins';
 
-import theme from './src/global/styles/theme'
-import { NavigationContainer } from '@react-navigation/native'
-import { AppRoutes } from './src/routes/app.routes'
-import { StatusBar } from 'expo-status-bar'
+import { AppRoutes } from './src/routes/app.routes';
+import theme from './src/global/styles/theme';
+
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
-  })
+  });
 
   if (!fontsLoaded) {
     return <AppLoading />
@@ -31,7 +34,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar style="light" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider> 
   )
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import AppleSvg from '../../assets/apple-icon.svg';
+import GoogleSvg from '../../assets/google-icon.svg';
+import LogoSvg from '../../assets/logo.svg';
+
 
 import {
   Container,
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
 } from './styles';
 
 export function SignIn() {
   return (
     <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg 
+            width={RFValue(120)}
+            height={RFValue(68)}
+          />
+
+          <Title>
+            Controle suas {'\n'}
+            finanças de forma {'\n'}
+            muito simples
+          </Title>
+        </TitleWrapper>
+
+        <SignInTitle>
+          Faça seu login com {'\n'}
+          uma das opções abaixo
+        </SignInTitle>
+      </Header>
+
+      <Footer>
+
+      </Footer>
+
     </Container>
   );
 }
