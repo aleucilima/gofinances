9db79d778c94c39d07d96da2d9bd6db5b24c0d16
aleucilima <aleuci.lima@gmail.com>
2022-05-13 23:00:42 -0300

    Creation signInWithGoogle function.

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -4,6 +4,8 @@ import React, {
   useContext 
 } from "react";
 
+import * as AuthSession from 'expo-auth-session';
+
 interface AuthProviderProps {
   children: ReactNode;
 }
@@ -26,6 +28,22 @@ function AuthProvider({ children }: AuthProviderProps) {
     id: '21323131',
     name: 'Alê',
     email: 'aleuci.lima@gmail.com'
+  }
+
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID = '';
+      const REDIRECT_URI = '';
+      const RESPONSE_TYPE = '';
+      const SCOPE = '';
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      AuthSession.startAsync({ authUrl });
+
+    } catch (error) {
+      throw new Error(error as any);
+    }
   }
 
   return (
