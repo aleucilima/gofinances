1af4df4f179f970400d4c7a093ed9f038f16136a
aleucilima <aleuci.lima@gmail.com>
2022-07-02 13:47:04 -0300

    update: install jest and testing-library.

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -4,10 +4,8 @@ import { render } from '@testing-library/react-native';
 import { Profile } from '../../screens/Profile';
 
 test('testanto o teste', () => {
-  const { getByPlaceholderText } = render(<Profile />);
+  const { debug } = render(<Profile />);
 
-  const inputName = getByPlaceholderText('Nome');
-  
-  expect(inputName.props.placeholder).toBeTruthy();
+  debug();
 });
 
