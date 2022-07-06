e2a4e6eb3dc8d79023f4d6017ea76f38b1bb08b8
aleucilima <aleuci.lima@gmail.com>
2022-07-05 23:01:02 -0300

    update: jest.config.js ans semantic tests

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
-describe('Profile', () => {
+describe('Profile Screen', () => {
   it('should have placeholder correctly in input user name', () => {
     const { getByPlaceholderText } = render(<Profile />);
 
@@ -12,7 +12,7 @@ describe('Profile', () => {
     expect(inputName).toBeTruthy();
   });
 
-  it('check if user data has ben loaded', () => {
+  it('should be load user data', () => {
     const { getByTestId } = render(<Profile />);
 
     const inputName = getByTestId('input-name');
@@ -22,7 +22,7 @@ describe('Profile', () => {
     expect(inputSurname.props.value).toEqual('Lima');
   });
 
-  it('check if title render correctly', () => {
+  it('should exist title correctly', () => {
     const { getByTestId } = render(<Profile />);
 
     const textTitle = getByTestId('text-tile');
